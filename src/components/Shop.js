5b39ebabd89f299393a4e4af7189f1585ffe2d7f
FaizUrRehman <shadowwalker3600@gmail.com>
2024-11-30 09:10:36 +0500

    State changing with BindActionCreators

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,17 +1,25 @@
 import React from 'react'
-import { useDispatch } from 'react-redux';
-// import { bindActionCreators } from 'redux';
+import { useDispatch, useSelector} from 'react-redux';
+import { bindActionCreators } from 'redux';
 import { actionCreator } from '../state';
 
 
 const Shop = () => {
   const dispatch = useDispatch();
+  const balace = useSelector(state=>state.amount)
+const {withdrawMoney, depostMoney} = bindActionCreators(actionCreator, dispatch)
   return (
     <div>
         <h2>Deposits/withdraw money</h2>
-      <button className="btn btn-primary mx-2" onClick={()=>{dispatch(actionCreator.withdrawMoney(100))}} >-</button>
+      {/* <button className="btn btn-primary mx-2" onClick={()=>{dispatch(actionCreator.withdrawMoney(100))}} >-</button>
       Update Balance
-      <button className="btn btn-primary mx-2" onClick={()=>{dispatch(actionCreator.depostMoney(100))}} >+</button>
+      <button className="btn btn-primary mx-2" onClick={()=>{dispatch(actionCreator.depostMoney(100))}} >+</button> */}
+
+    {/* with Bind action creator  */}
+
+      <button className="btn btn-primary mx-2" onClick={()=>{withdrawMoney(100)}} >-</button>
+      Update Balance ({balace})
+      <button className="btn btn-primary mx-2" onClick={()=>{depostMoney(100)}} >+</button>
     </div>
   )
 }
